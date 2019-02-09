e0428c89664c4976e5f4fd866469da2e1373af46
ragashree54 <47472072+ragashree54@users.noreply.github.com>
2019-02-09 15:42:11 +0530

    Create Posi.c

---
+++ Posi.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+
+int main(void) {
+	
+signed int n;
+	
+scanf("%d",&n);
+	
+if(n==0)
+	
+{
+		
+printf("\n zero");
+	
+}
+	
+else if(n>0)
+	
+{
+		
+printf("\n positive");
+	
+}
+	
+else if(n<0)
+	
+{
+		
+printf("\n negative");
+	
+}
+	
+else{
+		
+printf("\n error");
+	
+}
+	
+return 0;
+
+}
+
+
